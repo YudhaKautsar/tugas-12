7076c648ca599df77f6552107ea1fa86f73b77a9
Yudha Kautsar <muhammad.kautsar@iconpln.co.id>
2023-02-15 15:31:03 +0700

    first commit

---
+++ tugas-12.js
@@ -0,0 +1,11 @@
+const buah = [ 'Pisang', 'Jeruk', 'Apel', 'Mangga' ];
+
+console.log(buah);
+
+const pisangHide = buah.shift();
+
+console.log(buah);
+
+const manggaHide = buah.pop();
+
+console.log(buah);
